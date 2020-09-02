a10e3c6f474dbe1a6ee7a8ac4f4321a73b3906d8
rokiroki259 <KANHIROKI259@gmail.com>
2020-09-02 22:33:46 +0900

    New group creation/editing function

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
   root "messages#index"
+=======
+  devise_for :users
+  root 'groups#index'
+  resources :users, only: [:edit, :update, :destroy]
+  resources :groups, only: [:index, :new, :create, :edit, :update]
+>>>>>>> Stashed changes
 end
